fcd8553c42d9966f0f9216053a9ecfd085d77a09
Marc Mengel <mengel@fnal.gov>
2024-02-11 13:24:51 -0600

    allow adding gfal2 as dependency to rucio-clients

--- packages/rucio-clients/package.py
+++ packages/rucio-clients/package.py
@@ -15,10 +15,13 @@ class RucioClients(PythonPackage):
 
     # maintainers = ["marcmengel","bari12"]
 
+    version("33.3.0", sha256="08c32a046ae7695f5785e712f7208633656e451b312eb518fd4a5b9c9736dd67") 
     version("33.2.0", sha256="307480b57feefe827e1fdab64daf1a95fea2f3b3ea1f12913a1d331af695f76f") 
     version("32.4.0", sha256="847f8db44f6f7f2c53cfb596afcefefd310c5791d5fbee86bc487d904c130801") 
     version("1.31.7", sha256="99f7c981a9aff726f7fbbc0774b53d9df15d6a33798afb14ea0b4b3acaeadc53")
-    version("1.29.12", sha256="4b3e11726e75f89a04b513be16a53e5b9f7d33023ae8f0b7aa9032a2a1b8ae75", preferred=True)
+    version("1.29.12", sha256="4b3e11726e75f89a04b513be16a53e5b9f7d33023ae8f0b7aa9032a2a1b8ae75")
+
+    variant("gfal2", default=False)
 
     depends_on("py-setuptools", type=("build"))
     depends_on("py-pip", type="build")
@@ -35,3 +38,4 @@ class RucioClients(PythonPackage):
     depends_on("py-python-swiftclient", type=("build","run"))
     depends_on("py-argcomplete", type=("build","run"))
     depends_on("py-python-magic", type=("build","run"))
+    depends_on("gfal2-python", type=("build","run"), when="+gfal2")
