bcfe6e47121f41bd85ce4346725b9fcbe082682d
Marc Mengel <mengel@fnal.gov>
2024-09-30 22:09:51 -0500

    after fixing collections.abc stuff

--- packages/fife-utils/package.py
+++ packages/fife-utils/package.py
@@ -12,7 +12,7 @@ class FifeUtils(Package):
     homepage = "https://cdcvs.fnal.gov/redmine/projects/fife-utils/wiki"
     url = "http://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/fife_utils.v3_6_1.tar"
 
-    version("3.7.3", sha256="d8eca1d329ba9df190105b280172e4f46ddf7e5be4edc63418fae2634631cf4c")
+    version("3.7.3", sha256="8e8d45da4911b85b41e6ce00b0107c1c51cc17b27e569157d2c3facd9c25c2e0")
     version("3.7.2", sha256="2b4344d821e5650eda011e66375c47f808b5584df4ae2a591f345a94cc45711b")
     version("3.7.0", sha256="8d0efe2e007b0bb821accfad85ad455026fbe59b19f27c80edeb224db66cc74c")
     version("3.6.1", sha256="7bb1e2aeb703cb9b3ed098416c035509be85896f3891bc87afa14e21fa7132f5")
