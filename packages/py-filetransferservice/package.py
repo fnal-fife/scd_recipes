20dd4b66d58f1829e367870e56a72e3fe3b3ca46
Marc Mengel <mengel@fnal.gov>
2024-11-22 13:58:33 -0600

    Installing filetransferservice on AL9 now, with suitable version specifiers

--- packages/py-filetransferservice/package.py
+++ packages/py-filetransferservice/package.py
@@ -7,16 +7,17 @@ from spack import *
 
 
 class PyFiletransferservice(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Fermi File Transfer Service -- transfer and declare files placed in a dropbox"""
 
     homepage = "http://cdcvs.fnal.gov/redmine/projects/filetransferservice"
     url = "http://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/filetransferservice.v6_3_5.tar"
 
     version(
-        "6_3_5",
+        "6.3.5",
         sha256="3ca8b2507d56b2a836cd8141c200a4618f9e22fd54d5478e69208bbfd0d21643",
     )
 
+    depends_on("python@2.7.0:2.7.99", type=("build","run"))
     depends_on("py-setuptools", type="build")
     depends_on("py-twisted", type=("build", "run"))
     depends_on("py-service-identity", type=("build", "run"))
@@ -24,4 +25,28 @@ class PyFiletransferservice(PythonPackage):
     depends_on("py-jinja2", type=("build", "run"))
     depends_on("py-crcmod", type=("build", "run"))
     depends_on("py-sam-cp", type="run")
-    depends_on("ifdhc cxxstd=default", type="run")
+    depends_on("ifdhc cxxstd=11", type="run")
+
+    @run_before('install')
+    def set_version(self):
+        import os
+        with open("python/fts/_version.py","w") as of:
+            of.write('version="{0}"'.format(self.spec.version))
+        try:
+            os.rmdir("sam-cp")
+        except:
+            pass
+        os.symlink(self.spec["py-sam-cp"].prefix, "sam-cp")
+
+#    @property
+#    def build_directory(self):
+#        """The root directory of the Python package.
+#
+#        This is usually the directory containing one of the following files:
+#
+#        * ``pyproject.toml``
+#        * ``setup.cfg``
+#        * ``setup.py``
+#        """
+#        return self.pkg.stage.source_path
+#
