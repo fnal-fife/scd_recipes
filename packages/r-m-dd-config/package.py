18f86b4181ace9377c9657cb73aeb96ba9b4c3e4
Marc Mengel <mengel@fnal.gov>
2025-07-14 15:12:18 -0500

    adding metacat version

--- packages/r-m-dd-config/package.py
+++ packages/r-m-dd-config/package.py
@@ -52,6 +52,8 @@ class RMDdConfig(BundlePackage):
     version("1.2")
     version("1.3")
     version("1.4")
+    version("1.5")
+    version("1.6")
 
     depends_on("data-dispatcher")
     depends_on("rucio-clients")
